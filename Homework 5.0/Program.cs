0ff48553bbacbbee8a1cd1ff97d922f9984dc337
VitaVoy <videofag@mail.ru>
2021-05-13 16:57:55 +0300

    Добавьте файлы проекта.

---
+++ Homework 5.0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System; //Виталий Войтов
+
+namespace Homework_5._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = Console.ReadLine(); // Здесь пользователь вводит пароль
+            if (login.Length > 1 && login.Length < 11) // Здесь идет проверка на длинну пароля
+            {
+                char[] array = login.ToCharArray(); // Здесь введенный пароль переводитс в массив знаков
+                for (int i = 0; i < array.Length; i++) // Дальше проверяется соответствие знаков цифрам или латинице
+                {
+                    if (i >= 0 && i <= 9)
+                    {
+                        for (char j = 'A'; j < array.Length; j++)
+                        {
+                            if (j >= 'A' && j <= 'z') ;
+
+                        }
+                        Console.WriteLine("Bingo"); // Но почему-то код не работает как надо. Хотя по отдельности - части работали. 
+                    }
+                }
+
+
+
+            }
+            else
+                Console.WriteLine("Введите логин еще раз");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Homework 5.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System; //Виталий Войтов
+using System.Linq;
+
+namespace Homework_5._1
+{
+    class delWord
+    {
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args) //Код взял из Инета
+        {
+            {
+                Console.WriteLine("Введите строку:");
+                string str = Console.ReadLine();
+                Console.WriteLine("Введите количество символов в слове:");
+                int n = int.Parse(Console.ReadLine()); //До этого момента мне код полностью понятен
+                var msv = str.Split(new char[] { ' ', ',', '.', '!', '?', '-' }).Where(x => x.Length == n).ToArray();// А вот здесь уже нет. 
+                foreach (var item in msv) // каким образом переменная item стала соответствовать переменной n.
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            {
+                // метод удаления слова заканчивающегося на определенный символ
+                //Write("Введите осмысленное сообщения: ");
+                //string s = ReadLine();
+                //Write("Введите символ: ");
+                //char x = ToChar(ReadLine());
+                //string p = "Удалены те слова которые заканчиваются на заданный символ {0}: {1}";
+                // Удалить из сообщения все слова, которые заканчиваются на заданный символ
+                //string[] div = s.Split(' ', ',', '.', '-', '!', '?');
+                //for (int i = 0; i < div.Length; i++)
+                //{
+                //    if (div[i].EndsWith(x.ToString()))
+                //    {
+                //        WriteLine(p, x, div[i]);
+                //    }
+                //}
+                // выход из программы
+                //WriteLine("Нажмите любую клавишу для выхода...");
+                //ReadKey(true);
+            }
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
